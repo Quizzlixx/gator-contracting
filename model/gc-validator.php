019c76061b906714ce3655329851d705b32b44e0
Kerrie Low <klow4@mail.greenriver.edu>
2020-03-02 15:47:49 -0500

    Added validation. Untested

--- model/gc-validator.php
+++ model/gc-validator.php
@@ -1,6 +1,151 @@
 <?php
 
+/**
+ * Class GcValidator
+ */
 class GcValidator
 {
+    /**
+     * @var
+     */
+    private $_f3;
+    /**
+     * @var array
+     */
+    private $_errors;
 
+    /**
+     * GcValidator constructor.
+     * @param $_f3
+     */
+    public function __construct($_f3)
+    {
+        $this->_f3 = $_f3;
+        $this->_errors = array();
+    }
+
+    /**
+     * @return array
+     */
+    public function getErrors()
+    {
+        return $this->_errors;
+    }
+
+    /**
+     * Checks whether the personal information form is valid
+     *
+     * @return bool
+     */
+    public function validPersonalInformation()
+    {
+        $this->validCompany($_POST['company']);
+        $this->validFirst($_POST['first']);
+        $this->validLast($_POST['last']);
+        $this->validPhone($_POST['phone']);
+        $this->validEmail($_POST['email']);
+        $this->validAddress($_POST['address']);
+        $this->validCity($_POST['city']);
+        $this->validZip($_POST['zip']);
+
+        // if the $_errors array is empty, then we have valid data
+        return empty($this->_errors);
+    }
+
+//    public function validUsername($username)
+//    {
+//        if (empty(trim($username))) {
+//            $this->_errors['company'] = "Username is required.";
+//        }
+//    }
+
+    /**
+     * @param $company
+     */
+    public function validCompany($company)
+    {
+        if (empty(trim($company))) {
+            $this->_errors['company'] = "Company name is required.";
+        }
+    }
+
+    /**
+     * Takes a variable and returns true if it is not empty and contains only letters
+     *
+     * @param $first
+     * @return void
+     */
+    public function validFirst($first)
+    {
+        // first name is required
+        if (empty(trim($first))) {
+            $this->_errors['first'] = "First name is required.";
+        }
+    }
+
+
+    /**
+     * Takes a variable and returns true if it is not empty and contains only letters
+     *
+     * @param $last
+     * @return void
+     */
+    public function validLast($last)
+    {
+        // last name is required
+        if (empty(trim($last))) {
+            $this->_errors['last'] = "Last name is required.";
+        }
+    }
+
+    /**
+     * @param $number
+     * @return void
+     */
+    public function validPhone($number)
+    {
+        if (!preg_match('/^(\d{3})\-?(\d{3})\-?(\d{4})$/', $number)) {
+            $this->_errors['phone'] = "Valid 10 digit phone number is required: xxx-xxx-xxxx";
+        }
+    }
+
+    /**
+     * @param $email
+     */
+    public function validEmail($email)
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->_errors['email'] = "Valid email is required: email@example.com";
+        }
+    }
+
+    /**
+     * @param $address
+     */
+    public function validAddress($address)
+    {
+        if (empty($address)) {
+            $this->_errors['address'] = "Please enter an address.";
+        }
+    }
+
+    /**
+     * @param $city
+     */
+    public function validCity($city)
+    {
+        if (empty($city)) {
+            $this->_errors['city'] = "Please enter a city.";
+        }
+    }
+
+    /**
+     * @param $zip
+     */
+    public function validZip($zip)
+    {
+        if (empty($zip)) {
+            $this->_errors['zip'] = "Please enter a zip code.";
+        }
+    }
 }
