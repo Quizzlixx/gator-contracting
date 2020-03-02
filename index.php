59344624ebf6b3da6f12c5bd5a165ded1158f903
Kerrie Low <klow4@mail.greenriver.edu>
2020-03-02 15:12:57 -0500

    PP. Registration routes added

--- index.php
+++ index.php
@@ -7,14 +7,26 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 //require the autoload file
-require_once ('vendor/autoload.php');
+require_once('vendor/autoload.php');
 
 //create an instance of the base class
 $f3 = Base::instance();
 
+// states array
+$f3->set('states', array('AL' => 'Alabama', 'AK' => 'Alaska', 'AZ' => 'Arizona', 'AR' => 'Arkansas',
+    'CA' => 'California', 'CO' => 'Colorado', 'CT' => 'Connecticut', 'DE' => 'Delaware', 'DC' => 'District of Columbia',
+    'FL' => 'Florida', 'GA' => 'Georgia', 'HI' => 'Hawaii', 'ID' => 'Idaho', 'IL' => 'Illinois', 'IN' => 'Indiana',
+    'IA' => 'Iowa', 'KS' => 'Kansas', 'KY' => 'Kentucky', 'LA' => 'Louisiana', 'ME' => 'Maine', 'MD' => 'Maryland',
+    'MA' => 'Massachusetts', 'MI' => 'Michigan', 'MN' => 'Minnesota', 'MS' => 'Mississippi', 'MO' => 'Missouri',
+    'MT' => 'Montana', 'NE' => 'Nebraska', 'NV' => 'Nevada', 'NH' => 'New Hampshire', 'NJ' => 'New Jersey',
+    'NM' => 'New Mexico', 'NY' => 'New York', 'NC' => 'North Carolina', 'ND' => 'North Dakota', 'OH' => 'Ohio',
+    'OK' => 'Oklahoma', 'OR' => 'Oregon', 'PA' => 'Pennsylvania', 'RI' => 'Rhode Island', 'SC' => 'South Carolina',
+    'SD' => 'South Dakota', 'TN' => 'Tennessee', 'TX' => 'Texas', 'UT' => 'Utah', 'VT' => 'Vermont', 'VA' => 'Virginia',
+    'WA' => 'Washington', 'WV' => 'West Virginia', 'WI' => 'Wisconsin', 'WY' => 'Wyoming'));
+
 $routes = new Routes($f3);
 //define a default route
-$f3->route('GET /', function() {
+$f3->route('GET /', function () {
     $GLOBALS['routes']->home();
 });
 
@@ -33,5 +45,13 @@ $f3->route('GET /jobs', function () {
 $f3->route('GET /login', function () {
     $GLOBALS['routes']->login();
 });
+
+$f3->route('GET /client-register', function () {
+    $GLOBALS['routes']->clientRegister();
+});
+
+$f3->route('GET /contractor-register', function () {
+    $GLOBALS['routes']->contractorRegister();
+});
 //run fat free
-$f3 -> run();
+$f3->run();
