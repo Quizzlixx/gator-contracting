5d191d9cbbce0ad72533c92533b72736703ef4c0
Kerrie Low <klow4@mail.greenriver.edu>
2020-03-03 18:24:15 -0500

    Scripts now included

---
+++ scripts/gator-scripts.js
@@ -0,0 +1,7 @@
+$('#phone').keyup(function()
+{
+    this.value = this.value.replace(/(\d{3})\-?(\d{3})\-?(\d{4})/g, '$1-$2-$3');
+});
+$('#zip').keyup(function () {
+    this.value = this.value.replace(/^\d{5}$|^\d{5}-\d{4}$/g, '$1-$2');
+})
