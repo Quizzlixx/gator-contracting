be6e2578123481cd55c5e9ba46a702cd8efbc80c
Kerrie Low <klow4@mail.greenriver.edu>
2020-03-11 21:19:26 -0400

    Removed zip code JS. Edited footer to address validator issue

--- scripts/gator-scripts.js
+++ scripts/gator-scripts.js
@@ -3,5 +3,5 @@ $('#phone').keyup(function()
     this.value = this.value.replace(/(\d{3})\-?(\d{3})\-?(\d{4})/g, '$1-$2-$3');
 });
 $('#zip').keyup(function () {
-    this.value = this.value.replace(/^\d{5}$|^\d{5}-\d{4}$/g, '$1-$2');
+    this.value = this.value.replace(/(^\d{5}$)|(^\d{5}-\d{4}$)/g);
 })
